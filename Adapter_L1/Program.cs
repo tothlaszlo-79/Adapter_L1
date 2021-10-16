c4cccfa9d465e8b86a5875d63a441938d07826db
Laszlo Toth <laszlo.toth@nissin.com>
2021-10-16 10:37:56 +0200

    Add project files.

---
+++ Adapter_L1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Adapter_L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter a = new Adapter("Nagyon", "Ügyes");
+           
+            Ügyfél ü = new Ügyfél(a);
+            ü.Kiír();
+            Console.ReadLine();
+        }
+
+        public class Személy
+        {
+            public Személy(string V, string U) { Vezetéknév = V; Utónév = U; }
+            public string Vezetéknév { get; set; }
+            public string Utónév { get; set; }
+        }
+
+        interface INév { 
+            string Vezetéknév { get; set; } 
+            string Utónév { get; set; } 
+            string TeljesNév { get; } 
+        }
+
+        class Ügyfél
+        {
+            private INév n; 
+            public Ügyfél(INév újNév) 
+            { n = újNév; }
+            public void Kiír()
+            {
+                Console.WriteLine(n.Vezetéknév + "\t" + n.Utónév + "\t" + "\t" + n.TeljesNév);
+            }
+        }
+        class Adapter : Személy, INév
+        {
+            public Adapter(string V, string U) : base(V, U) { }
+            public string TeljesNév
+            {
+                get
+                {
+                    return Vezetéknév + " " + Utónév;
+                }
+            }
+        }
+
+    }
+
+    
+}
---
+++ Kiegészítés/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Kiegészítés
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Személy a = new Személy("Nagyon", "Ügyes");
+            
+            Ügyfél ü = new Ügyfél(a);
+            ü.Kiír();
+            Console.ReadLine();
+        }
+
+        public partial class Személy
+        {
+            public Személy(string V, string U) { Vezetéknév = V; Utónév = U; }
+            public string Vezetéknév { get; set; }
+            public string Utónév { get; set; }
+        }
+
+        public partial class Személy : INév
+        {
+            public string TeljesNév
+            {
+                get { return Vezetéknév + " " + Utónév; }
+            }
+        }
+
+        interface INév
+        {
+            string Vezetéknév { get; set; }
+            string Utónév { get; set; }
+            string TeljesNév { get; }
+        }
+
+        class Ügyfél
+        {
+            private INév n;
+            public Ügyfél(INév újNév)
+            { n = újNév; }
+            public void Kiír()
+            {
+                Console.WriteLine(n.Vezetéknév + "\t" + n.Utónév + "\t" + "\t" + n.TeljesNév);
+            }
+        }
+        public class Adapter : INév
+        {
+            Személy Személy;
+            public string TeljesNév
+            {
+                get { return Személy.Vezetéknév + " " + Személy.Utónév; }
+            }
+            public string Vezetéknév
+            {
+                get { return Személy.Vezetéknév; }
+                set { Személy.Vezetéknév = value; }
+            }
+            public string Utónév
+            {
+                get { return Személy.Utónév; }
+                set { Személy.Utónév = value; }
+            }
+            public Adapter(string V, string U)
+            {
+                Személy = new Személy(V, U);
+            }
+        }
+    }
+}
